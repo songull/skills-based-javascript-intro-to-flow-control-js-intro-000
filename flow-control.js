34a9063bafd97d2795475a97805d4aadbfc716f8
Kyler Barber <kylerbarber820@yahoo.com>
2019-06-04 00:12:31 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -21,11 +21,8 @@ return "You are a teenager";
 } else {return "You are not a teenager";}
 }
 
-function switchAge(age) {switch(age){
-case (age >= 13 && age <=19):
-console.log("You are a teenager");
-break;
-default:
-console.log("You have an age");
-}
+function switchAge(age) {if (age >= 13 && age <= 19) {
+return "You are a teenager";
+} else { (age > 19 && age < 13)
+return "You have an age";}
 }
