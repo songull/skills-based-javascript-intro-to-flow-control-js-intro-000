e8aac518cd7489e7eb7feb3ffb2bde6badb3c4d7
Kyler Barber <kylerbarber820@yahoo.com>
2019-06-04 00:00:46 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -2,8 +2,9 @@ function basicTeenager(age) { if(13<19)
   {age='you are a teenager!'};
 }
 
-function teenager(age) {
-
+function teenager(age) {if (age>=13 && age <= 19){
+return "You are a teenager!";
+} else {return "You are not a teenager";}
 }
 
 function ageChecker(age) {
