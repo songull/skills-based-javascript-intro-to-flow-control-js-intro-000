e7f523d94ffc4884d81af11b9d66cd96ce273c25
Kyler Barber <kylerbarber820@yahoo.com>
2019-06-04 00:02:11 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -15,8 +15,9 @@ return "You are a kid";
 }
 }
 
-function ternaryTeenager(age) {
-
+function ternaryTeenager(age) {if (age >= 13 && age <= 19){
+return "You are a teenager";
+} else {return "You are not a teenager";}
 }
 
 function switchAge(age) {
